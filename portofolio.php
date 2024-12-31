343fda5bf0501704bb286ebe96108ce95d693d66
Reza <162874480+halleludja@users.noreply.github.com>
2024-12-31 22:23:44 +0700

    Create portofolio.php

---
+++ portofolio.php
@@ -0,0 +1,97 @@
+ <!-- Product Section -->
+ <section class="py-5">
+        <div class="container">
+            <h2 class="text-center mb-5">Portofolio</h2>
+            <div class="row row-cols-2 row-cols-md-4 g-4">
+                <!-- Product Cards -->
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/jkt-hackathon.jpeg" class="card-img-top" alt="2024 Jakarta Hackathon">
+                        <div class="card-body">
+                            <h6 class="card-title">2024 Jakarta Hackathon</h6>
+                            <p class="small">Build an E-commerce for Sustainibility.</p>
+                            <p class="text-primary fw-bold">2nd Place</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/2sto.jpeg" class="card-img-top" alt="2022 2STO Demo Day">
+                        <div class="card-body">
+                            <h6 class="card-title">2022 2STO Demo Day</h6>
+                            <p class="small">High performance for all judges and mentors.</p>
+                            <p class="text-primary fw-bold">1st Place</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/msib.jpg" class="card-img-top" alt="MSIB Kemendikbud Tahun 2024">
+                        <div class="card-body">
+                            <h6 class="card-title">MSIB Kemendikbud Tahun 2024</h6>
+                            <p class="small">Lulus dengan kategori Tim Terbaik dan Komunikatif.</p>
+                            <p class="text-primary fw-bold">1st Place</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/techfest.jpeg" class="card-img-top" alt="2022 Itaewon TechFest">
+                        <div class="card-body">
+                            <h6 class="card-title">2022 Itaewon TechFest</h6>
+                            <p class="small">Developed a RESTful API using Node.js & Express.</p>
+                            <p class="text-primary fw-bold">3rd Place</p>
+                            <a href="#" class="btn btn-sm btn-primary">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/dataviz.webp" class="card-img-top" alt="2022 Uni Emirat Arab Data Vizualization">
+                        <div class="card-body">
+                            <h6 class="card-title">2022 Uni Emirat Arab Data Vizualization</h6>
+                            <p class="small">Created an interactive and analyze COVID-19 dashboard with Tableau and Python.</p>
+                            <p class="text-primary fw-bold">Top 5</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/6.jpeg" class="card-img-top" alt="2020 SandBox StartUp">
+                        <div class="card-body">
+                            <h6 class="card-title">2020 SandBox StartUp</h6>
+                            <p class="small">Designed and developed Mobile App with Flutter.</p>
+                            <p class="text-primary fw-bold">Finalist</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/8.jpeg" class="card-img-top" alt="2023 Designathon UI/UX">
+                        <div class="card-body">
+                            <h6 class="card-title">2023 Designathon UI/UX</h6>
+                            <p class="small">Collaborate and prototype platform using Figma and Adobe XD.</p>
+                            <p class="text-primary fw-bold">Finalist</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="images/images.jpeg" class="card-img-top" alt="2019 Kuala Lumpur WeekHacks">
+                        <div class="card-body">
+                            <h6 class="card-title">2019 Kuala Lumpur WeekHacks</h6>
+                            <p class="small">Participated and culminating successful simulation of ethical hacking for system.</p>
+                            <p class="text-primary fw-bold">Participant</p>
+                            <a href="#" class="btn btn-sm btn-primary">View Details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
